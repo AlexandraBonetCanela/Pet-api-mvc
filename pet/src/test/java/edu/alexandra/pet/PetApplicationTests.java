540cde21c314e170ebf57f589e604914b1f3875d
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-03-05 17:17:55 +0100

    project base

---
+++ pet/src/test/java/edu/alexandra/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alexandra.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
